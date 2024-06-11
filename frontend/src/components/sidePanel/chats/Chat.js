6e94381982d554c9151ba6199db90560b5a8171b
Oleksiy Krylchuk <krylchuk93@gmail.com>
2024-06-11 13:57:16 +0300

    frontend updates

--- frontend/src/components/sidePanel/chats/Chat.js
+++ frontend/src/components/sidePanel/chats/Chat.js
@@ -1,19 +1,31 @@
-
+import friendAvatar from "../../../images/avatar.png";
 
 
 const Chat = ({convo}) => {
 
 
     return (
-        <div className="chatList">
-            {convo.lastMessage.text}
+        <li className="">
+
+            <div className="item">
+                <img src={friendAvatar} alt=""/>
+                <div className="texts">
+                    <span>{convo.name}</span>
+                    <p>{convo.lastMessage.text}</p>
+                </div>
+
+                {/*Right*/}
+                <div className="add-chat">
+                    <span className="add-chat">
+                        {convo.lastMessage?.createdAt}
+                    </span>
+                </div>
+            </div>
 
 
+        </li>
 
-
-        </div>
-
-    );
+);
 }
 
 export default Chat;
